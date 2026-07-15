2fab1a82090e99412f74eaa3126629218a547b58
Menú-Digital-El Gordo & La Flaca <inyacir@gmail.com>
2026-07-14 20:11:55 -0600

    feat: make admin login the home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MenuProvider } from './context/MenuContext';
 import { CartProvider } from './context/CartContext';
-import { Home } from './components/Home';
 import { MenuPage } from './components/MenuPage';
 import { AdminGate } from './components/AdminGate';
-
-function HomeRoute() {
-  const navigate = useNavigate();
-
-  return (
-    <Home 
-      onNavigateToMenu={() => navigate('/menu')}
-    />
-  );
-}
-
-function AdminRoute() {
-  return <AdminGate />;
-}
 
 function App() {
   return (
@@ -25,9 +10,9 @@ function App() {
       <CartProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<HomeRoute />} />
+            <Route path="/" element={<AdminGate />} />
             <Route path="/menu" element={<MenuPage />} />
-            <Route path="/admin" element={<AdminRoute />} />
+            <Route path="/admin" element={<AdminGate />} />
           </Routes>
         </BrowserRouter>
       </CartProvider>
